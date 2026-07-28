6bfa788d8ed283ad55f96df78b10a1829b7a56b9
Abhijit-kumar-0 <abhijitkumar736900@gmail.com>
2026-07-28 22:21:33 +0530

    linear search

---
+++ DSA_ABHI/lecture-33_recursion_III/03_que_linear_search.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+bool isFound(int arr[], int n, int target) {
+    if(n == 0){
+        return false;
+    }
+
+    if(arr[0] == target) {
+        return true;
+    }
+    else {
+        int remainingPart = isFound(arr+1, n-1, target);
+        return remainingPart;
+    }
+
+}
+
+int main() {
+    int arr[6] = {3,6,-2,7,0,-4};
+    int target;
+    cin >> target;
+    int size = sizeof(arr)/sizeof(arr[0]);
+
+    int ans = isFound(arr, size, target);
+
+    if(ans) 
+        cout << "found " << endl;
+    else
+        cout << "not found " << endl;
+    
+
+    
+    return 0;
+}
