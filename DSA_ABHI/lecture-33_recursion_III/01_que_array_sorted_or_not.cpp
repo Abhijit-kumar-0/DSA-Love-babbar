a377969cec80091e7af17d11833c3f1144b0f490
Abhijit-kumar-0 <abhijitkumar736900@gmail.com>
2026-07-28 20:54:53 +0530

    problem solve recursion topic

--- DSA_ABHI/lecture-33_recursion_III/01_que_array_sorted_or_not.cpp
+++ DSA_ABHI/lecture-33_recursion_III/01_que_array_sorted_or_not.cpp
@@ -1,27 +1,62 @@
+// #include<iostream>
+// using namespace std;
+
+// bool CheckSort(int arr[], int size) {
+    
+//     if(size == 1) {
+//         return true;
+//     }
+//     if(arr[size-1] < arr[size-2]) {
+//         return false;
+//     }
+//     size--;
+//     // recursive relation
+//     CheckSort(arr, size);
+
+// }
+
+// int main() {
+
+//     int arr[6] = {2,4,6,9,11,13};
+
+//     int ans = CheckSort(arr, 6);
+     
+//     cout << "ans: " << ans << endl;
+    
+//     return 0;
+// }
+
+// method 2
+
 #include<iostream>
 using namespace std;
 
-bool CheckSort(int arr[], int size) {
-    
-    if(size == 1) {
-        return true;
-    }
-    if(arr[size-1] < arr[size-2]) {
-        return false;
-    }
-    size--;
-    // recursive relation
-    CheckSort(arr, size);
+    bool CheckSort(int arr[], int n) {
 
-}
+        // basecase
+        if(n == 0 || n == 1) {
+            return true;
+        }
 
+        if(arr[0] > arr[1]) {
+            return false;
+        }
+        else {
+            int checksort = CheckSort(arr + 1, n - 1);
+            return checksort;
+        }
+
+    }
 int main() {
 
-    int arr[6] = {2,4,6,9,11,13};
-
+    int arr[6] = {2,4,6,9,10,13};
     int ans = CheckSort(arr, 6);
-     
-    cout << "ans: " << ans << endl;
-    
+
+    if(ans) {
+        cout << "array is short " << endl;
+    }
+    else {
+        cout << "array is not short" << endl;
+    }
     return 0;
 }
--- DSA_ABHI/lecture-33_recursion_III/02_all_idx_value_add_HW.cpp
+++ DSA_ABHI/lecture-33_recursion_III/02_all_idx_value_add_HW.cpp
@@ -1,17 +1,50 @@
+// #include <iostream>
+// using namespace std;
+
+// int SumArr(int arr[], int n) {
+//     // basecase
+//     if(n==0 || n==1) {
+//         return arr[0];
+//     }
+
+//     //process
+//     arr[1] += arr[0];
+
+//     //RR
+//     int result = SumArr(arr+1, n-1);
+//     return result;
+// }
+
+// int main() {
+//     int arr[] = {3,2,5,1,6};
+//     int n = sizeof(arr)/sizeof(arr[0]);
+
+//     cout << SumArr(arr, n);
+//     return 0;
+// }
+
+
 #include <iostream>
-#include<vector>
-#include <algorithm>  // for sort
 using namespace std;
 
 int SumArr(int arr[], int n) {
     // basecase
-    
+    if(n==0) {
+        return 0;
+    }
+    if(n == 1) {
+        return arr[0];
+    }
+    int RemainingPart = SumArr(arr+1, n-1);
+    int sum = arr[0] + RemainingPart;
+    return sum;
 }
 
 int main() {
-    int arr[] = {5, 2, 9, 1, 5, 6};
+    int arr[] = {3,2,5,1,6};
     int n = sizeof(arr)/sizeof(arr[0]);
 
-    SumArr(arr, n);
+    int sum = SumArr(arr, n);
+    cout << "Sum " << sum;
     return 0;
 }
