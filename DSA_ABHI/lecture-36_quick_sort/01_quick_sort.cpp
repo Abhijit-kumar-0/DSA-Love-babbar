f95c34536d84d2ba2314ca7ddfc8d6f78ec24c55
Abhijit-kumar-0 <abhijitkumar736900@gmail.com>
2026-08-07 21:44:19 +0530

    lec-35 exe file

--- DSA_ABHI/lecture-36_quick_sort/01_quick_sort.cpp
+++ DSA_ABHI/lecture-36_quick_sort/01_quick_sort.cpp
@@ -2,46 +2,66 @@
 using namespace std;
 
 int partition(int* arr, int s, int e) {
-
-    int p = s;
+    int pivotIdx = s;
     int min = arr[s];
 
     for(int i = s; i<e; i++) {
         if(arr[i] < min) {
-            min = arr[i];
-            p++;
+            pivotIdx++;
         }
     }
 
+
+    // place the right position of the given pivot
+    swap(arr[s], arr[pivotIdx]);
+
+    // less then or greater then pivot element shift 
+    int i = s;
+    int j = e;
+    int pivot = arr[pivotIdx];
+    while(pivotIdx > i && pivotIdx < j) {
+        while(arr[i] <= pivot) {
+            i++;
+        }
+        while(arr[j] >= pivot) {
+            j--;
+        }
+
+        if(i < pivotIdx && j > pivotIdx) {
+            swap(arr[i], arr[j]);
+            i++;
+            j--;
+        }
+
+    }
+
+    return pivotIdx;
 }
 
 void quicksort(int* arr, int s, int e) {
+
+    int p = partition(arr, s, e);
 
     if(s >= e) {
         return;
     }
 
-    int p = partition(arr, s, e);
-        swap(arr[s], arr[p]);
-
-    for(int i = s; i<e; i++) {
-        cout << arr[i] << " ";
-    }cout << endl;
 
     // left part
-    quicksort(arr, s, p-1);
+    quicksort(arr, s, p);
 
     // right part
-    quicksort(arr, p+1, e);
+    quicksort(arr, ++p, e);
 }
 
 int main(){
 
-    int arr[7] = {0,9,2,4,6,-1,-4};
+    int arr[7] = {10,80,30,90,40,50,70};
     int size = sizeof(arr)/ sizeof(arr[0]);
 
     quicksort(arr, 0, size);
-
+    
+    cout << "final result: " << endl;
     for(int i = 0; i<size; i++) {
         cout << arr[i] << " ";
 
