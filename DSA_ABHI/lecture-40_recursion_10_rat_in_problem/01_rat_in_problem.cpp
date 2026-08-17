0629fdc527b602c89d05a7ebe04f50dc526462dc
Abhijit-kumar-0 <abhijitkumar736900@gmail.com>
2026-08-17 22:15:27 +0530

    to add vector & string

---
+++ DSA_ABHI/lecture-40_recursion_10_rat_in_problem/01_rat_in_problem.cpp
@@ -0,0 +1,17 @@
+ #include<iostream>
+ #include<vector>
+ using namespace std;
+ 
+ int main(){
+ vector<vector<int>> m = {{1,0,0,0},{1,1,0,1},{1,1,0,0},{0,1,1,1}};
+ int n = 4;
+
+ vector<string> ans;
+ if(m[0][0] == 0) {
+   cout << "empty!" << endl;
+ }
+
+
+
+ return 0;
+ }
