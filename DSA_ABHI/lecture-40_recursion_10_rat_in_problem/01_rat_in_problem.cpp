cdb43bfb6f335135ae5538d3dc8a143c002319ae
Abhijit-kumar-0 <abhijitkumar736900@gmail.com>
2026-08-21 15:17:34 +0530

    fix the error

--- DSA_ABHI/lecture-40_recursion_10_rat_in_problem/01_rat_in_problem.cpp
+++ DSA_ABHI/lecture-40_recursion_10_rat_in_problem/01_rat_in_problem.cpp
@@ -4,7 +4,7 @@
  using namespace std;
 
 bool isSafe(int x, int y, int n, vector<vector<int>> visited, vector<vector<int>>& m) {
-  if((x>0 && x <n) && (y > 0 && y<n) && visited[x][y] == 0 && m[x][y] == 1) {
+  if((x >= 0 && x < n) && (y >= 0 && y < n) && visited[x][y] == 0 && m[x][y] == 1) {
     return true;
   }
   else {
@@ -29,7 +29,7 @@ void solve(vector<vector<int>>& m, int n, vector<string>& ans, int x, int y, vec
   int newy = y;
   if(isSafe(newx, newy, n, visited, m)) {
     path.push_back('D');
-    solve(m, n, ans, x, y, visited, path);
+    solve(m, n, ans, x+1, y, visited, path);
     path.pop_back();
   }
   
@@ -38,25 +38,25 @@ void solve(vector<vector<int>>& m, int n, vector<string>& ans, int x, int y, vec
   newy = y-1;
   if(isSafe(newx, newy, n, visited, m)) {
     path.push_back('L');
-    solve(m, n, ans, x, y, visited, path);
+    solve(m, n, ans, x, y-1, visited, path);
     path.pop_back();
   }
-
+  
   // Right
   newx = x;
   newy = y+1;
   if(isSafe(newx, newy, n, visited, m)) {
     path.push_back('R');
-    solve(m, n, ans, x, y, visited, path);
+    solve(m, n, ans, x, y+1, visited, path);
     path.pop_back();
   }
   
   // UP
-  newx = x+1;
+  newx = x-1;
   newy = y;
   if(isSafe(newx, newy, n, visited, m)) {
     path.push_back('U');
-    solve(m, n, ans, x, y, visited, path);
+    solve(m, n, ans, x-1, y, visited, path);
     path.pop_back();
   }
 
@@ -64,8 +64,8 @@ void solve(vector<vector<int>>& m, int n, vector<string>& ans, int x, int y, vec
 }
 
  int main(){
- vector<vector<int>> m = {{1,0,0,0}
-                          ,{1,1,0,1}
+ vector<vector<int>> m = {{1,1,0,0}
+                          ,{0,1,0,1}
                           ,{1,1,0,0}
                           ,{0,1,1,1}};
  int n = 4;
@@ -73,6 +73,7 @@ void solve(vector<vector<int>>& m, int n, vector<string>& ans, int x, int y, vec
  vector<string> ans;
  if(m[0][0] == 0) {
     cout << "empty!" << endl;
+    return 0;
  }
 
  int srcx = 0;
@@ -80,8 +81,8 @@ void solve(vector<vector<int>>& m, int n, vector<string>& ans, int x, int y, vec
 
  vector<vector<int>> visited = m;
 
- for(int i(0); i<n; i++) {
-  for(int j(0); i<n; j++) {
+ for(int i = 0; i<n; i++) {
+  for(int j = 0; j<n; j++) {
     visited[i][j] = 0;
   }
  }
@@ -89,9 +90,9 @@ void solve(vector<vector<int>>& m, int n, vector<string>& ans, int x, int y, vec
  string path = "";
  solve(m,n,ans,srcx,srcy,visited,path);
  sort(ans.begin(), ans.end());
- 
- for(auto i : ans) {
-  cout << i << " ";
+ cout << " Hi " << ans.size() << endl;
+ for(int i = 0; i < ans.size(); i++) {
+  cout << ans[i] << " ";
  }
  return 0;
  }
